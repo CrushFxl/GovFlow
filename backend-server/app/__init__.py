ebc53890988008c9264312901101f92202c95423
Cushxl <2428977170@qq.com>
2025-08-22 15:41:40 +0800

    修复因同源策略导致的cookie签发失败的问题

--- backend-server/app/__init__.py
+++ backend-server/app/__init__.py
@@ -12,6 +12,8 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     app.config['SESSION_TYPE'] = 'filesystem'
+    # 设置cookie的SameSite属性为None，以支持跨站点使用
+    app.config['SESSION_COOKIE_SAMESITE'] = 'None'
     CORS(app, supports_credentials=True)
     db.init_app(app)
 
